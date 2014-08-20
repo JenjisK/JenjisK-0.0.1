fee44e00d6f46c4a85e16d46b7007174c37e3fd0
Victor Drouin <voblivion@hotmail.fr>
2014-08-20 16:30:56 +0200

    Page de test pour segment.intersectCircle
    Une page avec un canvas pour un test déjà préparé.

---
+++ Sources/test.js
@@ -0,0 +1,59 @@
+/* WARNING */
+/* Ces quelques fonctions sont provisoires et devront être recodées/renommées */
+// Retourne la position de l'objet par rapport au coin haut-gauche de la page
+function z_object_position(obj)
+{
+	var position = new Vector(0,0);
+	do
+	{
+		position = position.plus(new Vector(obj.offsetLeft, obj.offsetTop));
+	}
+	while(obj = obj.offsetParent);
+	
+	return position.duplicate();
+}
+// Retourne la position du clique sur l'objet relativement à sa position
+function z_object_click_position(object, e)
+{
+	var position_object = new Vector(object.offsetLeft, object.offsetTop);
+	var position = new Vector(e.clientX, e.clientY);
+	return position.minus(position_object);
+}
+// Pour le moment ne fait rien
+function approx(a)
+{
+	return a;
+}
+function depprox(a)
+{
+	return a;
+}
+// Trace un cercle
+CanvasRenderingContext2D.prototype.drawCircle = function(circle)
+{
+	// On crée le cercle
+	this.beginPath();
+	if(typeof circle.center != "undefined")
+	{
+		this.arc(circle.center.x, circle.center.y, circle.radius, 0, 2 * Math.PI, false);
+	}
+	
+	// On affiche le cercle
+	this.strokeStyle = 'rgb(0,0,0)';
+	this.stroke();
+}
+CanvasRenderingContext2D.prototype.drawSegment = function(segment)
+{
+	// On crée le segment
+	this.beginPath();
+	this.moveTo(segment.u.x, segment.u.y);
+	this.lineTo(segment.v.x, segment.v.y);
+	
+	// On affiche le cercle
+	this.strokeStyle = 'rgb(0,0,0)';
+	this.stroke();
+}
+CanvasRenderingContext2D.prototype.clear = function()
+{
+	this.clearRect(0,0,1000, 1000);
+}
