efc278cb41b06390fd926418125b2cef1df96568
Victor Drouin <voblivion@hotmail.fr>
2014-08-22 14:19:05 +0200

    Shape.intersectShape : DONE ! TO TEST Again and Again

--- Sources/Geometry/Shape.js
+++ Sources/Geometry/Shape.js
@@ -21,7 +21,7 @@ function Polygon()
 	this.type = "Polygon";
 }
 /* Héritage déclaré */
-Polygon.prototype = new Path();
+Polygon.prototype = new Shape();
 /* Circle est directement hérité de Shape */
 function Circle()
 {
@@ -33,79 +33,96 @@ function Circle()
 Circle.prototype = new Shape();
 
 Shape.prototype.intersectShape = function(shape){
-	switch(this.type)
+	switch(shape.type)
 	{
 		case 'polygon' :
-			switch(shape.type)
-			{
-				case 'polygon' : return Polygon.intersectPolygon(this, shape); break;
-				case 'path' : return Polygon.intersectPath(this, shape); break;
-				case 'circle' : return Polygon.intersectCircle(this, shape); break;
-				default : console.group('Shape.intersectShape');console.error('Type Of Shape Undefined');console.log(shape);console.groupEnd('Shape.intersectShape');break;
-			}
-			break;
+			return this.intersectPolygon(shape); break;
 		case 'path' :
-			switch(shape.type)
-			{
-				case 'polygon' : return Polygon.intersectPath(shape, this); break;
-				case 'path' : return Path.intersectPath(this, shape); break;
-				case 'circle' : return Path.intersectCircle(this, shape); break;
-				default : console.group('Shape.intersectShape');console.error('Type Of Shape Undefined');console.log(shape);console.groupEnd('Shape.intersectShape');break;
-			}
-			break;
+			return this.intersectPath(shape); break;
 		case 'circle' :
-			switch(shape.type)
-			{
-				case 'polygon' : return Polygon.intersectCircle(shape, this); break;
-				case 'path' : return Path.intersectCircle(shape, this); break;
-				case 'circle' : return Circle.intersectCircle(this, shape); break;
-				default : console.group('Shape.intersectShape');console.error('Type Of Shape Undefined');console.log(shape);console.groupEnd('Shape.intersectShape');break;
-			}
-			break;
-		default : console.group('Shape.intersectShape');console.error('Type Of Shape Undefined');console.log(this);console.groupEnd('Shape.intersectShape');break;
+			return this.intersectCircle(shape); break;
+		default : console.group('Shape.intersectShape');console.error('Type Of Shape Undefined');console.log(shape);console.groupEnd('Shape.intersectShape');break;
 	}
 }
-Circle.prototype.intersectShape = function(shape){
-	if (shape.type == "Path" || shape.type == "Polygon") {	
-		for (var i = 0; i < shape.vertices.length-1; i++) {
-			var v1 = shape.vertices[i];
-			var v2 = shape.vertices[i+1];
+Polygon.prototype.intersectPolygon = function(polygon){
+	var l = this.vertices.length, m = polygon.vertices.length;
+	for(var i = 0; i < l; i++){
+		var segment1 = new Segment(this.vertices[i], this.vertices[(i+1)%l]);
+		
+		for(var j = 0; j < m; j++){
+			var segment2 = new Segment(polygon.vertices[j], polygon.vertices[(j+1)%m]);
 			
-			var tmpSegment = new Segment(v1, v2);
-			
-			if (tmpSegment.intersectCircle(this) != null) {
+			if(segment1.intersectSegment(segment2) != null){
 				return true;
 			}
-		}
-		return false;
-	} else if (shape.type == "Circle") {
-		if ((this.center - shape.center).norm() < this.radius + shape.radius) {
+		}			
+	}
+	return false;
+}
+Polygon.prototype.intersectPath = function(path){
+	var l = this.vertices.length;
+	for(var i = 0; i < l; i++){
+		var segment1 = new Segment(this.vertices[i], this.vertices[(i+1)%l]);
+		
+		for(var j = 0; j < path.vertices.length-1; j++){
+			var segment2 = new Segment(path.vertices[j], path.vertices[(j+1)%m]);
+			
+			if(segment1.intersectSegment(segment2) != null){
+				return true;
+			}
+		}			
+	}
+	return false;
+}
+Polygon.prototype.intersectCircle = function(circle){
+	var l = this.vertices.length;
+	for(var i = 0; i < l; i++){
+		var segment = new Segment(this.vertices[i], this.vertices[(i+1)%l]);
+		
+		if (segment.intersectCircle(circle) != null){
 			return true;
-		} else {
-			return false;
 		}
+	}
+}
+Path.prototype.intersectPolygon = function(polygon){
+	return polygon.intersectPath(this);
+}
+Path.prototype.intersectPath = function(path){
+	for(var i = 0; i < this.vertices.length-1; i++){
+		var segment1 = new Segment(this.vertices[i], this.vertices[i+1]);
+		
+		for(var j = 0; j < path.vertices.length-1; j++){
+			var segment2 = new Segment(path.vertices[j], path.vertices[j+1]);
+			
+			if(segment1.intersectSegment(segment2) != null){
+				return true;
+			}
+		}			
+	}
+	return false;
+}
+Path.prototype.intersectCircle = function(circle){
+	for(var i = 0; i < this.vertices.length-1; i++){
+		var segment = new Segment(this.vertices[i], this.vertices[i+1]);
+		
+		if (segment.intersectCircle(circle) != null){
+			return true;
+		}
+	}
+}
+Circle.prototype.intersectPolygon = function(polygon){
+	return polygon.intersectCircle(this);
+}
+Circle.prototype.intersectPath = function(path){
+	return path.intersectCircle(this);
+}
+Circle.prototype.intersectCircle = function(circle){
+	if((Vector.minus(this.center,circle.center).norm() < this.radius + circle.radius){
+		return true;
+	}else{
+		return false;
 	}
 }
 
-Path.prototype.intersectShape = function(shape) {
-	if (shape.type == "Path" || shape.type == "Polygon") {	
-		for (var i = 0; i < shape.vertices.length-1; i++) {
-			var v1 = shape.vertices[i];
-			var v2 = shape.vertices[i+1];
-			var tmpSegment = new Segment(v1, v2);
-			
-			for (var j = 0; j < this.vertices.length-1; j++) {
-				var v3 = this.vertices[j];
-				var v4 = this.vertices[j+1];
-				var tmpSegment2 = new Segment(v3, v4);
-				
-				if (tmpSegment.intersectSegment(tmpSegment2) != null) {
-					return true;
-				}
-			}			
-		}
-		return false;
-	} else if (shape.type == "Circle") {
-		return shape.intersectShape(this);
-	}
-}
+
+
